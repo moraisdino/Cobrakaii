76dfdee347932dfb7064ed7c674366c2c1e4d19c
moraisdino <169068802+moraisdino@users.noreply.github.com>
2024-05-13 21:11:48 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+     System.out.println("-----------");
+     System.out.println("|COBRA KAI|");
+     System.out.println("-----------");
+     System.out.println("");
+     System.out.println("Cobra Kai é uma série que revive a rivalidade entre Daniel LaRusso e Johnny Lawrence, personagens do icônico filme dos anos 80 Karate Kid. Ambientada décadas após os eventos do filme original, a primeira temporada acompanha Johnny, que revive o famoso dojo Cobra Kai, onde ele treinou nos anos 80, na tentativa de recuperar seu rumo na vida. Enquanto isso, Daniel, agora um bem-sucedido revendedor de carros, enfrenta desafios pessoais e profissionais. A série explora temas de redenção, identidade e o legado do karatê, enquanto mergulha nas vidas desses personagens e da nova geração de jovens que se envolvem na rivalidade entre Cobra Kai e Miyagi-Do. ");
+   System.out.println("");
+    System.out.println("link do treiler: https://youtu.be/gfDTor2B5ck?si=BUU6oGzvoxsr83wq");
+  }
+}
